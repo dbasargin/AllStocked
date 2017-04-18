7cbbcee9cf4364cf6e81adce48d346e00a4f4f8c
dbasargin <dennisbasargin@hotmail.com>
2017-04-17 19:48:38 -0700

    recent changes to navigation. Updated buttons css. created categories scaffolding. resized homepage logo.

---
+++ AllStocked/Controllers/CategoriesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AllStocked;
+using AllStocked.Models;
+
+namespace AllStocked.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private AllStockedDBEntities db = new AllStockedDBEntities();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            if (SessionHelper.IsMemberLoggedIn())
+            {
+                int currentId = Convert.ToInt32(HttpContext.Session["AccountID"]);
+                var categories = db.Categories.Where(p => p.AccountID == currentId).Include(c => c.Account);
+            return View(categories.ToList());
+            }
+            else
+            {
+                //if current session doesnt exist redirect user to home/register page.
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccountID = new SelectList(db.Accounts, "AccountID", "AccountName");
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,AccountID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccountID = new SelectList(db.Accounts, "AccountID", "AccountName", category.AccountID);
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccountID = new SelectList(db.Accounts, "AccountID", "AccountName", category.AccountID);
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,AccountID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccountID = new SelectList(db.Accounts, "AccountID", "AccountName", category.AccountID);
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
