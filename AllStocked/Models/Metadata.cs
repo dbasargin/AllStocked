fd6b5c87da52c413773f4f36f7f0d8cceda29197
dbasargin <dennisbasargin@hotmail.com>
2017-05-30 08:56:58 -0700

    home work

--- AllStocked/Models/Metadata.cs
+++ AllStocked/Models/Metadata.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-using Foolproof;
 using System.ComponentModel;
 
 namespace AllStocked
@@ -17,10 +16,10 @@ namespace AllStocked
         [DisplayName("Product Name")]
         public string ProductName { get; set; }
 
+        [GreaterThan("Demand", ErrorMessage = "Must be larger than Demand")]
         [Range(0, Int32.MaxValue, ErrorMessage = "Cannot be below 0")]
         public int Par { get; set; }
 
-        [LessThan("Par",ErrorMessage ="test")]
         [Range(0, Int32.MaxValue, ErrorMessage = "Cannot be below 0")]
         public int Demand { get; set; }
 
@@ -34,5 +33,59 @@ namespace AllStocked
         public virtual Category Category { get; set; }
     }
 
-    
+    public class GreaterThanAttribute : ValidationAttribute
+    {
+
+        public GreaterThanAttribute(string otherProperty)
+            : base("{0} must be greater than {1}")
+        {
+            OtherProperty = otherProperty;
+        }
+
+        public string OtherProperty { get; set; }
+
+        public string FormatErrorMessage(string name, string otherName)
+        {
+            return string.Format(ErrorMessageString, name, otherName);
+        }
+
+        protected override ValidationResult
+            IsValid(object firstValue, ValidationContext validationContext)
+        {
+            var firstComparable = firstValue as IComparable;
+            var secondComparable = GetSecondComparable(validationContext);
+
+            if (firstComparable != null && secondComparable != null)
+            {
+                if (firstComparable.CompareTo(secondComparable) < 1)
+                {
+                    object obj = validationContext.ObjectInstance;
+                    var thing = obj.GetType().GetProperty(OtherProperty);
+                    var displayName = (DisplayAttribute)Attribute.GetCustomAttribute(thing, typeof(DisplayAttribute));
+
+                    return new ValidationResult(
+                        FormatErrorMessage(validationContext.DisplayName));
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        protected IComparable GetSecondComparable(
+            ValidationContext validationContext)
+        {
+            var propertyInfo = validationContext
+                                  .ObjectType
+                                  .GetProperty(OtherProperty);
+            if (propertyInfo != null)
+            {
+                var secondValue = propertyInfo.GetValue(
+                    validationContext.ObjectInstance, null);
+                return secondValue as IComparable;
+            }
+            return null;
+        }
+    }
+
+
 }
