8fa5db367b3bc402b385d5bff92ab06ffd2f9674
dbasargin <dennisbasargin@hotmail.com>
2017-11-27 20:58:53 -0800

    test 3

--- AllStocked/Models/Creds.cs
+++ AllStocked/Models/Creds.cs
@@ -8,6 +8,6 @@ namespace AllStocked.Models
     //Gets credentials needed for the applications
     public class Creds
     {
-        //TESt 2: to See if ignore is working here
+        //TESt 3: to See if ignore is working here
     }
 }
