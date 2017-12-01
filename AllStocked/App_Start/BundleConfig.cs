31d56da4d1c5c16b1426037e9f7fbb2d610e8351
dbasargin <dennisbasargin@hotmail.com>
2017-11-30 19:25:41 -0800

    Added partial class descriptions to Settings Page

--- AllStocked/App_Start/BundleConfig.cs
+++ AllStocked/App_Start/BundleConfig.cs
@@ -27,7 +27,7 @@ namespace AllStocked
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
